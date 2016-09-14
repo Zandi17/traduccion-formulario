db5e8583f5db2d9aa30966f1833fab8dda377e89
Sandy Ciquero <sandy-ciqueroch@gmail.com>
2016-09-14 14:17:12 -0500

    cambio a ingles

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.getElementsByTagName('h2')[0].innerHTML='Ingresa tu cuenta';
+document.getElementById('inputEmail').placeholder='Correo Electronico';
+document.getElementById('inputPassword').placeholder='Contraseña';
+document.querySelector('span').innerHTML = 'Recordar Datos';
+document.querySelector('button').innerHTML = 'Iniciar Sesion';
